67ef748b4da96586107c30d827a13532b7f80e35
Thuyen Ngo <vanthuyen@gmail.com>
2017-03-18 13:24:26 -0700

    unify interface for tensors and variables

--- README.md
+++ README.md
@@ -3,27 +3,23 @@
 ### Supported OPs
 `fft1`, `fft2`, `fft3`, `ifft1`, `ifft2`, `ifft3`.
 
-### For Tensors
+### Examples
 ````
-from torchsignal import fft
+from torchsignal import fft1, fft2
+from torch.autograd import Variable
 
 # [..., n, 2] array represents complex numbers
 x = torch.rand(5, 2).cuda()
-y = fft.fft1(x)
+y = fft1(x)
 
 # [..., h, w, 2] array represents complex images
 x = torch.rand(5, 128, 128, 2).cuda()
-y = fft.fft2(x)
-````
+y = fft2(x)
 
-### For Variables
-````
-from torch.autograd import Variable
-from torchsignal import functional as F
-
+# For variables
 x = Variable(torch.rand(5, 2).cuda())
-y = F.fft1(x)
+y = fft1(x)
 
 x = Variable(torch.rand(5, 128, 128, 2).cuda())
-y = F.fft2(x)
+y = fft2(x)
 ````
--- torchsignal/__init__.py
+++ torchsignal/__init__.py
@@ -1,1 +1,39 @@
+import torch
 from siglib import fft, functional
+
+def fft1(input):
+    if isinstance(input, torch.autograd.Variable):
+        return functional.fft1(input)
+    else:
+        return fft.fft1(input)
+
+
+def fft2(input):
+    if isinstance(input, torch.autograd.Variable):
+        return functional.fft2(input)
+    else:
+        return fft.fft2(input)
+
+def fft3(input):
+    if isinstance(input, torch.autograd.Variable):
+        return functional.fft3(input)
+    else:
+        return fft.fft3(input)
+
+def ifft1(input):
+    if isinstance(input, torch.autograd.Variable):
+        return functional.ifft1(input)
+    else:
+        return fft.ifft1(input)
+
+def ifft2(input):
+    if isinstance(input, torch.autograd.Variable):
+        return functional.ifft2(input)
+    else:
+        return fft.ifft2(input)
+
+def ifft3(input):
+    if isinstance(input, torch.autograd.Variable):
+        return functional.ifft3(input)
+    else:
+        return fft.ifft3(input)
